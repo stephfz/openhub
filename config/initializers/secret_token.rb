3b1b4e22757e3622804580149632c225f0c9b365
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-01-15 18:06:47 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openhub::Application.config.secret_token = '572f354e5095f09af11e0a046c4985d6e9cdf9758f8794c3e89ed785519b17d618d2ca47cbfe4195fee66db5fb99e095c6a9a0aa686798944e82390104e80d75'
