33d4ab5642f5d29166ee7ecfc912b0eac5d0496a
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-01-15 22:48:40 -0500

    Adding sign in/out routes & methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Openhub::Application.routes.draw do
   get "openhub/index"
 
+  match "/auth/:provider/callback" => "sessions#create"
+  match "/signout" => "sessions#destroy", :as => :signout
+
   root to: "openhub#index"
 end
